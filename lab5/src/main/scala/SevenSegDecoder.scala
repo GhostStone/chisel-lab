8e4299ac18ce28165d114f712990b40e4ac1a033
Man-Gee <contact@yeungmang.ee>
2023-03-09 14:56:08 +0100

    new

--- lab5/src/main/scala/SevenSegDecoder.scala
+++ lab5/src/main/scala/SevenSegDecoder.scala
@@ -12,6 +12,58 @@ class SevenSegDecoder extends Module {
 
   // ***** your code starts here *****
 
+  switch(io.sw){
+
+    is("h0".U){
+      sevSeg := ("h3f".U)
+    }
+    is("h1".U){
+      sevSeg := ("h06".U)
+    }
+    is("h2".U){
+      sevSeg := ("h5b".U)
+    }
+    is("h3".U){
+      sevSeg := ("h4f".U)
+    }
+    is("h4".U){
+      sevSeg := ("h66".U)
+    }
+    is("h5".U){
+      sevSeg := ("h6d".U)
+    }
+    is("h6".U){
+      sevSeg := ("h7d".U)
+    }
+    is("h7".U){
+      sevSeg := ("h07".U)
+    }
+    is("h8".U){
+      sevSeg := ("h7f".U)
+    }
+    is("h9".U){
+      sevSeg := ("h6f".U)
+    }
+    is("ha".U){
+      sevSeg := ("h77".U)
+    }
+    is("hb".U){
+      sevSeg := ("h7c".U)
+    }
+    is("hc".U){
+      sevSeg := ("h39".U)
+    }
+    is("hd".U){
+      sevSeg := ("h5e".U)
+    }
+    is("he".U){
+      sevSeg := ("h79".U)
+    }
+    is("hf".U){
+      sevSeg := ("h71".U)
+    }
+  }
+
   // ***** your code ends here *****
 
   io.seg := ~sevSeg
